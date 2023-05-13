94421c7e4ac7b69f3daa112a3b33056371754ab9
Salim Kemal Tirit <salim.tirit@gmail.com>
2023-05-13 17:21:00 +0300

    user info view of the application

---
+++ lib/user_info.dart
@@ -0,0 +1,46 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:mini_insta_stories/user.dart';
+
+class UserInfo extends StatelessWidget {
+  final User user;
+
+  const UserInfo({
+    required Key key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 20.0,
+          backgroundColor: Colors.grey[300],
+          backgroundImage: CachedNetworkImageProvider(
+            user.profileImageUrl,
+          ),
+        ),
+        const SizedBox(width: 10.0),
+        Expanded(
+          child: Text(
+            user.name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        IconButton(
+          icon: const Icon(
+            Icons.close,
+            size: 30.0,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ],
+    );
+  }
+}
