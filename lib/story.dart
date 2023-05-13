fdcb3cbcc993c44923a38a30c19d1013baba25b5
Salim Kemal Tirit <salim.tirit@gmail.com>
2023-05-13 17:20:42 +0300

    story class with url, duration and if is video

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  final String url;
+  final bool isVideo;
+  final Duration duration;
+
+  Story({
+    required this.url,
+    required this.isVideo,
+    required this.duration,
+  });
+}
