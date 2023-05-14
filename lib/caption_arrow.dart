71b573051aefc83f18a0f78695026943fc326de5
Salim Kemal Tirit <salim.tirit@gmail.com>
2023-05-15 01:29:57 +0300

    launch the url in the caption when the user presses the arrow

--- lib/caption_arrow.dart
+++ lib/caption_arrow.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CaptionArrow extends StatelessWidget {
+  final url;
+
   const CaptionArrow({
     required Key key,
+    required this.url,
   }) : super(key: key);
+
+  Future<void> _launchUrl(url) async {
+    Uri _url = Uri.parse(url);
+    if (!await launchUrl(_url, mode: LaunchMode.externalApplication)) {
+      throw Exception('Could not launch $_url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Align(
+    return Align(
       alignment: Alignment.bottomCenter,
-      child: Icon(
-        Icons.keyboard_arrow_up,
-        color: Colors.white,
-        size: 75,
+      child: GestureDetector(
+        onTap: () => _launchUrl(url),
+        child: Icon(
+          Icons.keyboard_arrow_up,
+          color: Colors.white,
+          size: 75,
+        ),
       ),
     );
   }
